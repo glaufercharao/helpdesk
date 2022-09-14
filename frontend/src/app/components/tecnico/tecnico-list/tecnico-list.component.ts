81fde39950307a3d6d5381468be1620d5205b281
Gláufer Charão <glaufer.charao@gmail.com>
2022-09-14 16:31:45 -0300

    Componente login criado. Adicionado table na listagem de tecnicos.

--- frontend/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ frontend/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {Tecnico} from "../../../models/Tecnico";
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  private ELEMENT_DATA: Tecnico[] = [{
+    id:1,
+    nome: 'Glaufer',
+    cpf: '807.822.480-04',
+    email: 'glaufer.charao@gmail.com',
+    perfis:['ADMIN','TECNICO'],
+    dataCriacao: '15/06/1982',
+  }];
+
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email','acoes'];
+
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
---
+++ frontend/src/app/models/Tecnico.ts
@@ -0,0 +1,9 @@
+export interface Tecnico {
+    id?: any;
+    nome: string;
+    cpf:  string;
+    email: string;
+    perfis: string[];
+    dataCriacao: any;
+
+}
